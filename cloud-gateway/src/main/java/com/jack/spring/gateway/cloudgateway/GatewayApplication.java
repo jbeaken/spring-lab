3bffda75d9172838017d51c5d32754b40da72978
jbeaken <jack747@gmail.com>
2020-02-15 12:55:59 +0000

    Removing security from httpbin service

--- cloud-gateway/src/main/java/com/jack/spring/gateway/cloudgateway/GatewayApplication.java
+++ cloud-gateway/src/main/java/com/jack/spring/gateway/cloudgateway/GatewayApplication.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.chaos.monkey.shopping.domain.Product;
 
-import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
+//import org.springframework.cloud.security.oauth2.gateway.TokenRelayGatewayFilterFactory;
 
 import static io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.SlidingWindowType.COUNT_BASED;
 import static java.time.Duration.ofMillis;
@@ -41,8 +41,8 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
-	@Autowired
-	private TokenRelayGatewayFilterFactory filterFactory;
+//	@Autowired
+//	private TokenRelayGatewayFilterFactory filterFactory;
 
 	@Bean
 	public RouteLocator routes(RouteLocatorBuilder builder) {
